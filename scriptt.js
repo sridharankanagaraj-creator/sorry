dd40bd16a2e1bffac10f50714cd03c57e2b0fc38
sridharankanagaraj-creator <sridharankanagaraj@gmail.com>
2026-01-31 16:42:17 +0530

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,5 @@
+function saveText() {
+  let text = document.getElementById("userText").value;
+  document.getElementById("output").innerText =
+    "💌 nee tha ma yenaku ellame nee illana naa illa di chello 💌:\n" + text;
+}
